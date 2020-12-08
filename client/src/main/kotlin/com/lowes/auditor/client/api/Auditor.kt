0ae289473be0b24fd8f0bed9480f6f0937055b9e
Deepankar Singh <deepankar.singh@lowes.com>
2020-12-08 05:36:02 +0530

    implementing interfaces

---
+++ client/src/main/kotlin/com/lowes/auditor/client/api/Auditor.kt
@@ -0,0 +1,8 @@
+package com.lowes.auditor.client.api
+
+import com.lowes.auditor.client.entities.domain.AuditorEventConfig
+
+interface Auditor {
+
+    fun audit(oldObject: Any?, newObject: Any?, auditorEventConfig: AuditorEventConfig)
+}
--- client/src/main/kotlin/com/lowes/auditor/client/api/AuditorService.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/api/AuditorService.kt
@@ -1,6 +1,17 @@
 package com.lowes.auditor.client.api
 
+import com.lowes.auditor.client.entities.domain.AuditorEventConfig
+import com.lowes.auditor.client.library.service.AuditEventGeneratorService
+import reactor.core.scheduler.Schedulers
+import javax.inject.Inject
 import javax.inject.Named
 
 @Named
-class AuditorService()
+internal class AuditorService @Inject constructor(
+    private val auditEventGeneratorService: AuditEventGeneratorService
+) : Auditor {
+    override fun audit(oldObject: Any?, newObject: Any?, auditorEventConfig: AuditorEventConfig) {
+        auditEventGeneratorService.audit(oldObject, newObject, auditorEventConfig)
+            .subscribeOn(Schedulers.newParallel("AuditorScheduler"))
+    }
+}
