7e443af1a79a07b04fbdd5188d90eefa39526f40
Deepankar Singh <deepankar.singh@lowes.com>
2020-12-10 01:29:16 +0530

    config update

--- client/src/main/kotlin/com/lowes/auditor/client/entities/domain/AuditorEventConfig.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/entities/domain/AuditorEventConfig.kt
@@ -1,19 +1,72 @@
 package com.lowes.auditor.client.entities.domain
 
+import org.springframework.context.annotation.Configuration
+
+@Configuration(proxyBeanMethods = false)
 data class AuditorEventConfig(
-    val applicationName: String? = null,
-    val eventSource: EventSource? = null,
-    val eventSubType: String? = null,
-    val eventSourceMetadata: EventSourceMetadata? = null,
-    val metadata: Map<String, String>? = null,
+    var applicationName: String? = null,
+    var eventSource: EventSourceConfig? = null,
+    var eventSubType: String? = null,
+    var metadata: Map<String, String>? = null,
+    var filters: Filters? = null,
+)
+
+@Configuration(proxyBeanMethods = false)
+data class Filters(
+    var event: EventFilter? = null,
+    var element: ElementFilter? = null,
+    var logging: LoggingFilter? = null,
+)
+
+@Configuration(proxyBeanMethods = false)
+data class EventFilter(
+    var enabled: Boolean = false,
+    var type: List<EventType>? = null,
+)
+
+@Configuration(proxyBeanMethods = false)
+data class ElementFilter(
+    var enabled: Boolean = false,
+    var types: List<String>? = null,
+    var options: ElementFilterOptions? = null,
+)
+
+@Configuration(proxyBeanMethods = false)
+data class LoggingFilter(
+    var enabled: Boolean = false,
+)
+
+@Configuration(proxyBeanMethods = false)
+data class ElementFilterOptions(
+    var includes: List<String>? = null,
+    var excludes: List<String>? = null,
+    var metaData: Map<String, String>? = null
+)
+
+@Configuration(proxyBeanMethods = false)
+data class EventSourceConfig(
+    var type: EventSourceType = EventSourceType.SYSTEM,
+    var metadata: EventSourceMetadataConfig? = null,
 ) {
-    fun copyIfNull(eventConfig: AuditorEventConfig): AuditorEventConfig {
-        return this.copy(
-            applicationName = this.applicationName ?: eventConfig.applicationName,
-            eventSource = this.eventSource ?: eventConfig.eventSource,
-            eventSubType = this.eventSubType ?: eventConfig.eventSubType,
-            eventSourceMetadata = this.eventSourceMetadata ?: eventConfig.eventSourceMetadata,
-            metadata = this.metadata ?: eventConfig.metadata,
+    fun toEventSource(): EventSource {
+        return EventSource(
+            type = this.type,
+            metadata = this.metadata?.toEventSourceMetadata()
+        )
+    }
+}
+
+@Configuration(proxyBeanMethods = false)
+data class EventSourceMetadataConfig(
+    var id: String? = null,
+    var email: String? = null,
+    var name: String? = null,
+) {
+    fun toEventSourceMetadata(): EventSourceMetadata {
+        return EventSourceMetadata(
+            id = this.id,
+            email = this.email,
+            name = this.name,
         )
     }
 }
--- client/src/main/kotlin/com/lowes/auditor/client/library/config/AuditorConfig.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/library/config/AuditorConfig.kt
@@ -1,33 +1,18 @@
 package com.lowes.auditor.client.library.config
 
 import com.lowes.auditor.client.entities.domain.AuditorEventConfig
-import com.lowes.auditor.client.entities.domain.EventSource
-import com.lowes.auditor.client.entities.domain.EventSourceMetadata
+import com.lowes.auditor.client.entities.domain.EventSourceConfig
+import com.lowes.auditor.client.entities.domain.EventSourceType
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration(proxyBeanMethods = false)
 @ConfigurationProperties(prefix = "auditor")
 internal data class AuditorConfig(
-    var config: Config = Config()
-) {
-    fun toAuditEventConfig(): AuditorEventConfig {
-        val config = this.config
-        return AuditorEventConfig(
-            applicationName = config.applicationName,
-            eventSource = config.eventSource,
-            eventSubType = config.eventSubType,
-            eventSourceMetadata = config.eventSourceMetadata,
-            metadata = config.metadata
+    var config: AuditorEventConfig = AuditorEventConfig(
+        applicationName = "NOT_CONFIGURED",
+        eventSource = EventSourceConfig(
+            type = EventSourceType.SYSTEM
         )
-    }
-}
-
-@Configuration(proxyBeanMethods = false)
-internal data class Config(
-    var applicationName: String = "NOT_CONFIGURED",
-    var eventSource: EventSource = EventSource.SYSTEM,
-    var eventSubType: String? = null,
-    var eventSourceMetadata: EventSourceMetadata? = null,
-    var metadata: Map<String, String>? = null,
+    )
 )
