363935e99b6274735d5863d658dcad8862f066ee
Deepankar Singh <deepankar.singh@lowes.com>
2020-12-11 14:03:52 +0530

    library and use case refactoring

--- client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
@@ -4,19 +4,18 @@ import com.lowes.auditor.client.entities.domain.AuditEvent
 import com.lowes.auditor.client.entities.domain.AuditorEventConfig
 import com.lowes.auditor.client.entities.interfaces.infrastructure.event.EventPublisher
 import com.lowes.auditor.client.entities.util.merge
-import com.lowes.auditor.client.library.config.AuditorConfig
 import com.lowes.auditor.client.usecase.ElementAggregatorUseCase
 import reactor.core.publisher.Flux
 
 internal class AuditEventGeneratorService(
     private val elementAggregatorUseCase: ElementAggregatorUseCase,
-    private val auditorConfig: AuditorConfig,
+    private val initialConfig: AuditorEventConfig,
     private val eventPublisher: EventPublisher,
     private val auditEventFilterService: AuditEventFilterService,
     private val auditEventDecoratorService: AuditEventDecoratorService,
 ) {
-    fun audit(oldObject: Any?, newObject: Any?, auditorEventConfig: AuditorEventConfig?): Flux<AuditEvent> {
-        val config = auditorEventConfig?.let { auditorConfig.config merge it } ?: auditorConfig.config
+    fun audit(oldObject: Any?, newObject: Any?, requestConfig: AuditorEventConfig?): Flux<AuditEvent> {
+        val config = requestConfig?.let { initialConfig merge it } ?: initialConfig
         val events = elementAggregatorUseCase.aggregate(oldObject, newObject, config)
         val decoratedEvents = auditEventDecoratorService.decorate(events, config, newObject)
         val filteredEvents = auditEventFilterService.filter(decoratedEvents, config)
