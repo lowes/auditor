765aa91e025f82db6c7878c61f5f6344130a06a6
Deepankar Singh <deepankar.singh@lowes.com>
2020-12-11 14:45:09 +0530

    adding java example

---
+++ client-example/src/main/java/fooprice/PriceApplication.java
@@ -0,0 +1,51 @@
+package fooprice;
+
+import com.lowes.auditor.client.api.Auditor;
+import fooprice.model.Price;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@SpringBootApplication
+public class PriceApplication implements CommandLineRunner {
+    private Auditor auditor;
+
+    public PriceApplication(Auditor auditor) {
+        this.auditor = auditor;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Price oldPrice = new Price(
+                UUID.randomUUID(),
+                1234,
+                "old_item",
+                null
+        );
+
+        UUID newItemNumber = UUID.randomUUID();
+        Price newPrice = new Price(
+                UUID.randomUUID(),
+                21212,
+                "new_item",
+                new HashMap<String, String>() {{
+                    put("new_price_id", "98767");
+                }}
+        );
+        System.out.println("Running auditor! for newItemNumber $newItemNumber");
+            auditor.audit(
+                    oldPrice,
+                    newPrice
+            );
+
+            System.out.println("Done");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceApplication.class, args);
+    }
+
+}
