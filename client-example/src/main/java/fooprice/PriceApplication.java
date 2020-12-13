ccabb8530368ad681437257bc0b26728ac4a6f88
Deepankar Singh <deepankar.singh@lowes.com>
2020-12-13 20:13:49 +0530

    fixing example and api gradle spec

--- client-example/src/main/java/fooprice/PriceApplication.java
+++ client-example/src/main/java/fooprice/PriceApplication.java
@@ -2,6 +2,7 @@ package fooprice;
 
 import com.lowes.auditor.client.api.Auditor;
 import fooprice.model.Price;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
