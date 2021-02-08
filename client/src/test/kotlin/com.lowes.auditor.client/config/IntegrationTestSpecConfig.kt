5650bbe44fa2d4c030c2ae6198d3dfcd6fdf5ccd
Deepankar Singh <deepankar.singh@lowes.com>
2021-02-08 14:49:17 +0530

    adding performance test and a simple not null/empty check

---
+++ client/src/test/kotlin/com.lowes.auditor.client/config/IntegrationTestSpecConfig.kt
@@ -0,0 +1,22 @@
+package com.lowes.auditor.client.config
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.listeners.Listener
+import io.kotest.core.spec.IsolationMode
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.toDuration
+
+object IntegrationTestSpecConfig : AbstractProjectConfig() {
+
+    override val parallelism = Runtime.getRuntime().availableProcessors() * 2
+
+    override val isolationMode: IsolationMode = IsolationMode.InstancePerTest
+
+    override val invocationTimeout = 60000L
+
+    @ExperimentalTime
+    override val timeout = 720.toDuration(DurationUnit.SECONDS)
+
+    override fun listeners(): List<Listener> = listOf()
+}
