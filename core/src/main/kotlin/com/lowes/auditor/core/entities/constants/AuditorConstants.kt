732b83dc8546346791cee2ac1dd191455bd3b76f
Deepankar Singh <deepankar.singh@lowes.com>
2021-03-08 13:45:54 +0530

    auditor config fix

--- core/src/main/kotlin/com/lowes/auditor/core/entities/constants/AuditorConstants.kt
+++ core/src/main/kotlin/com/lowes/auditor/core/entities/constants/AuditorConstants.kt
@@ -1,6 +1,6 @@
 package com.lowes.auditor.core.entities.constants
 
 const val AUDITOR = "auditor"
-const val LOWESISS_AUDITOR = "lowesiss".plus(AUDITOR)
+const val LOWESISS_AUDITOR = "lowesiss".plus(".").plus(AUDITOR)
 const val NOT_CONFIGURED = "NOT_CONFIGURED"
 const val AUDITOR_PRODUCER = "auditor-client-iss-producer"
