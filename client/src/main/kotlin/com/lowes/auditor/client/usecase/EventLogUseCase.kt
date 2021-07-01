f63e9cd9b8b1db854f00d6b60865cd082391b711
Deepankar Singh <deepankar.singh@lowes.com>
2021-07-02 01:47:20 +0530

    adding event log use case

---
+++ client/src/main/kotlin/com/lowes/auditor/client/usecase/EventLogUseCase.kt
@@ -0,0 +1,33 @@
+package com.lowes.auditor.client.usecase
+
+import com.lowes.auditor.client.entities.domain.AuditorEventConfig
+import com.lowes.auditor.client.entities.domain.EventSourceConfig
+import com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks.ObjectLogGenerator
+import com.lowes.auditor.client.entities.util.orDefault
+import com.lowes.auditor.core.entities.domain.AuditEvent
+import com.lowes.auditor.core.entities.domain.EventType
+import reactor.core.publisher.Mono
+import java.time.OffsetDateTime
+import java.time.ZoneId
+import java.util.UUID
+
+class EventLogUseCase(
+    private val objectLogGenerator: ObjectLogGenerator
+) {
+    fun logEvent(entity: Any, auditorEventConfig: AuditorEventConfig): Mono<AuditEvent> {
+        return objectLogGenerator.generate(entity)
+            .map {
+                AuditEvent(
+                    id = UUID.randomUUID(),
+                    applicationName = auditorEventConfig.applicationName.orDefault("NOT_CONFIGURED"),
+                    timestamp = OffsetDateTime.now(ZoneId.of("UTC")),
+                    type = EventType.CREATED,
+                    source = auditorEventConfig.eventSource.orDefault(EventSourceConfig()).toEventSource(),
+                    elements = emptyList(),
+                    subType = auditorEventConfig.eventSubType,
+                    metadata = auditorEventConfig.metadata,
+                    log = it
+                )
+            }
+    }
+}
