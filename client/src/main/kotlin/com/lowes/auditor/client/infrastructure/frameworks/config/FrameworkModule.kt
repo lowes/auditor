5d870602d90ee9fdfa7217d791befeea82099a94
Deepankar Singh <deepankar.singh@lowes.com>
2021-07-02 01:46:35 +0530

    adding ObjectLogGenerator implementation

--- client/src/main/kotlin/com/lowes/auditor/client/infrastructure/frameworks/config/FrameworkModule.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/infrastructure/frameworks/config/FrameworkModule.kt
@@ -1,8 +1,10 @@
 package com.lowes.auditor.client.infrastructure.frameworks.config
 
 import com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks.ObjectDiffChecker
+import com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks.ObjectLogGenerator
 import com.lowes.auditor.client.infrastructure.frameworks.service.DefaultLogProvider
 import com.lowes.auditor.client.infrastructure.frameworks.service.ObjectDiffCheckerService
+import com.lowes.auditor.client.infrastructure.frameworks.service.ObjectLogGeneratorService
 import com.lowes.auditor.core.entities.util.JsonObject
 
 object FrameworkModule {
@@ -13,5 +15,9 @@ object FrameworkModule {
 
     val defaultLogProvider: DefaultLogProvider by lazy {
         DefaultLogProvider()
+    }
+
+    val objectLogGenerator: ObjectLogGenerator by lazy {
+        ObjectLogGeneratorService(JsonObject.objectMapper)
     }
 }
---
+++ client/src/main/kotlin/com/lowes/auditor/client/infrastructure/frameworks/service/ObjectLogGeneratorService.kt
@@ -0,0 +1,14 @@
+package com.lowes.auditor.client.infrastructure.frameworks.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks.ObjectLogGenerator
+import reactor.core.publisher.Mono
+
+class ObjectLogGeneratorService(
+    private val objectMapper: ObjectMapper
+) : ObjectLogGenerator {
+
+    override fun generate(entity: Any): Mono<String> {
+        return Mono.fromCallable { objectMapper.writeValueAsString(entity) }
+    }
+}
