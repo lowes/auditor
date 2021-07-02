e363a3abb745269c03988ba77dcd8f071d6fca56
Deepankar Singh <deepankar.singh@lowes.com>
2021-07-02 14:54:00 +0530

    adding logs track failures

--- client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
@@ -5,7 +5,9 @@ import com.lowes.auditor.client.entities.interfaces.infrastructure.event.EventPu
 import com.lowes.auditor.client.entities.util.merge
 import com.lowes.auditor.client.usecase.ElementAggregatorUseCase
 import com.lowes.auditor.client.usecase.EventLogUseCase
+import com.lowes.auditor.client.usecase.LoggingFilterUseCase
 import com.lowes.auditor.core.entities.domain.AuditEvent
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 
 class AuditEventGeneratorService(
@@ -16,12 +18,16 @@ class AuditEventGeneratorService(
     private val auditEventDecoratorService: AuditEventDecoratorService,
     private val eventLogUseCase: EventLogUseCase,
 ) {
+    private val logger = LoggerFactory.getLogger(LoggingFilterUseCase::class.java)
+
     fun audit(oldObject: Any?, newObject: Any?, requestConfig: AuditorEventConfig?): Flux<AuditEvent> {
         val config = requestConfig?.let { initialConfig merge it } ?: initialConfig
         val events = elementAggregatorUseCase.aggregate(oldObject, newObject, config)
         val decoratedEvents = auditEventDecoratorService.decorate(events, config, newObject)
         val filteredEvents = auditEventFilterService.filter(decoratedEvents, config)
-        return eventPublisher.publishEvents(filteredEvents).flatMap { events }
+        return eventPublisher.publishEvents(filteredEvents)
+            .flatMap { events }
+            .doOnError { logger.error("audit event failed", it) }
     }
 
     fun log(entity: Any, requestConfig: AuditorEventConfig?): Flux<AuditEvent> {
@@ -29,6 +35,8 @@ class AuditEventGeneratorService(
         val events = Flux.from(eventLogUseCase.logEvent(entity, config))
         val decoratedEvents = auditEventDecoratorService.decorate(events, config, entity)
         val filteredEvents = auditEventFilterService.filter(decoratedEvents, config)
-        return eventPublisher.publishEvents(filteredEvents).flatMap { events }
+        return eventPublisher.publishEvents(filteredEvents)
+            .flatMap { events }
+            .doOnError { logger.error("audit log event failed", it) }
     }
 }
