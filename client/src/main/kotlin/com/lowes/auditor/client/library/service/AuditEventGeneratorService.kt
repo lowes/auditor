d8b8284a7df639e74df9979831c60b4e9134a136
Deepankar Singh <deepankar.singh@lowes.com>
2021-10-25 11:47:49 +0530

    retrying and logging implmentation

--- client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/library/service/AuditEventGeneratorService.kt
@@ -2,13 +2,19 @@ package com.lowes.auditor.client.library.service
 
 import com.lowes.auditor.client.entities.domain.AuditorEventConfig
 import com.lowes.auditor.client.entities.interfaces.infrastructure.event.EventPublisher
+import com.lowes.auditor.client.entities.util.TEN
+import com.lowes.auditor.client.entities.util.THIRTY
 import com.lowes.auditor.client.entities.util.merge
+import com.lowes.auditor.client.entities.util.orDefault
 import com.lowes.auditor.client.usecase.ElementAggregatorUseCase
 import com.lowes.auditor.client.usecase.EventLogUseCase
 import com.lowes.auditor.client.usecase.LoggingFilterUseCase
 import com.lowes.auditor.core.entities.domain.AuditEvent
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
+import reactor.util.retry.RetryBackoffSpec
+import reactor.util.retry.RetrySpec
+import java.time.Duration
 
 class AuditEventGeneratorService(
     private val elementAggregatorUseCase: ElementAggregatorUseCase,
@@ -25,9 +31,11 @@ class AuditEventGeneratorService(
         val events = elementAggregatorUseCase.aggregate(oldObject, newObject, config)
         val decoratedEvents = auditEventDecoratorService.decorate(events, newObject)
         val filteredEvents = auditEventFilterService.filter(decoratedEvents, config)
+        val logMessage = "audit event failed"
         return eventPublisher.publishEvents(filteredEvents)
+            .retryWhen(doRetry(config, logMessage))
+            .doOnError { logger.error(logMessage, it) }
             .flatMap { events }
-            .doOnError { logger.error("audit event failed", it) }
     }
 
     fun log(entity: Any, requestConfig: AuditorEventConfig?): Flux<AuditEvent> {
@@ -35,8 +43,24 @@ class AuditEventGeneratorService(
         val events = Flux.from(eventLogUseCase.logEvent(entity, config))
         val decoratedEvents = auditEventDecoratorService.decorate(events, entity)
         val filteredEvents = auditEventFilterService.filter(decoratedEvents, config)
+        val logMessage = "audit log event failed"
         return eventPublisher.publishEvents(filteredEvents)
+            .retryWhen(doRetry(config, logMessage))
+            .doOnError { logger.error(logMessage, it) }
             .flatMap { events }
-            .doOnError { logger.error("audit log event failed", it) }
+    }
+
+    private fun doRetry(config: AuditorEventConfig, message: String): RetryBackoffSpec {
+        return RetrySpec.fixedDelay(
+            config.retry?.count.orDefault(TEN.toLong()),
+            config.retry?.delay.orDefault(Duration.ofSeconds(THIRTY.toLong()))
+        )
+            .doBeforeRetry {
+                logger.info(
+                    "$message, hence retrying. exception:{}, retryCount:{}",
+                    it.failure(),
+                    it.totalRetries()
+                )
+            }
     }
 }
