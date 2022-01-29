1aa590350d3d9ca1c8bcbe1ac8a2757ab9f85e91
Deepankar Singh <deepankar.singh@lowes.com>
2022-01-29 23:12:52 +0530

    adding kdocs to eixtsing usecases classes

--- client/src/main/kotlin/com/lowes/auditor/client/usecase/ElementAggregatorUseCase.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/usecase/ElementAggregatorUseCase.kt
@@ -5,15 +5,27 @@ import com.lowes.auditor.client.entities.domain.EventSourceConfig
 import com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks.ObjectDiffChecker
 import com.lowes.auditor.client.entities.util.orDefault
 import com.lowes.auditor.core.entities.domain.AuditEvent
+import com.lowes.auditor.core.entities.domain.Element
 import com.lowes.auditor.core.entities.domain.EventType
 import reactor.core.publisher.Flux
 import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.util.UUID
 
+/**
+ * Compares old and new objects to generates [AuditEvent]
+ * @property objectDiffChecker instanc of [ObjectDiffChecker] that performs the actual diff between two objects
+ */
 class ElementAggregatorUseCase(
     private val objectDiffChecker: ObjectDiffChecker
 ) {
+    /**
+     * Compares old and new objects to find delta changes called [Element] which is then aggregated to [AuditEvent]
+     * @param oldObject previous instance of the object to be compared against
+     * @param newObject latest instance of the object that needs to be audited
+     * @param auditorEventConfig an instance of [AuditorEventConfig] containing audit related configurations
+     * @return flux of generated [AuditEvent]
+     */
     fun aggregate(oldObject: Any?, newObject: Any?, auditorEventConfig: AuditorEventConfig): Flux<AuditEvent> {
         return objectDiffChecker.diff(oldObject, newObject)
             .groupBy {
