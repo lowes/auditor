1b12824e981388a8c8fa32f39e3eab628d2d4111
Deepankar Singh <deepankar.singh@lowes.com>
2022-01-29 23:10:44 +0530

    adding comments to client spring boot start module

--- client-spring-boot-starter/src/main/kotlin/com/lowes/auditor/client/springboot/library/config/AuditorAutoConfig.kt
+++ client-spring-boot-starter/src/main/kotlin/com/lowes/auditor/client/springboot/library/config/AuditorAutoConfig.kt
@@ -6,9 +6,18 @@ import com.lowes.auditor.core.entities.constants.AUDITOR
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
+/**
+ * Data class containing configurations for [AuditorEventConfig] and [AuditEventProducerConfig].
+ * It is initialized implicitly by [AuditorAutoConfigureModule] by reading configurations present
+ * in the application config file when auto configurations are enabled.
+ *
+ * @property config instance of [AuditorEventConfig]
+ * @property producer instance of [AuditEventProducerConfig]
+ * @constructor creates an instance of AuditorAutoConfig
+ */
 @Configuration(proxyBeanMethods = false)
 @ConfigurationProperties(prefix = AUDITOR)
-class AuditorAutoConfig {
-    var config: AuditorEventConfig = AuditorEventConfig.getDefaultInstance()
-    var producer: AuditEventProducerConfig = AuditEventProducerConfig()
-}
+data class AuditorAutoConfig (
+    val config: AuditorEventConfig = AuditorEventConfig.getDefaultInstance(),
+    val producer: AuditEventProducerConfig = AuditEventProducerConfig()
+)
--- client-spring-boot-starter/src/main/kotlin/com/lowes/auditor/client/springboot/library/config/AuditorAutoConfigureModule.kt
+++ client-spring-boot-starter/src/main/kotlin/com/lowes/auditor/client/springboot/library/config/AuditorAutoConfigureModule.kt
@@ -9,18 +9,31 @@ import org.springframework.context.annotation.Configuration
 import reactor.core.scheduler.Scheduler
 import reactor.core.scheduler.Schedulers
 
+/**
+ * Spring boot's autoconfigure module to initialize and load [Auditor] instances
+ */
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(AuditorAutoConfig::class)
 class AuditorAutoConfigureModule(
     private val auditorAutoConfig: AuditorAutoConfig
 ) {
 
+    /**
+     * Creates a [Scheduler] instance exclusively for managing audits
+     * @return a reactor scheduler instance
+     */
     @Bean
     @ConditionalOnMissingBean
     fun auditorServiceScheduler(): Scheduler {
         return Schedulers.newParallel("auditorServiceScheduler")
     }
 
+    /**
+     * Creates an [Auditor] instance that is injected to application spring lifecycle
+     * @param elementFilters list of [AuditEventElementFilter]
+     * @param auditorServiceScheduler initialized auditor Scheduler
+     * @return an [Auditor] instance
+     */
     @Bean
     fun auditor(elementFilters: List<AuditEventElementFilter>, auditorServiceScheduler: Scheduler): Auditor {
         return Auditor.getInstance(
