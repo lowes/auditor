39967b0fd81e179fcd339e3a213adc35cb830389
Deepankar Singh <deepankar.singh@lowes.com>
2022-01-30 11:16:43 +0530

    correcting description for ObjectLogGenerator interface

--- client/src/main/kotlin/com/lowes/auditor/client/entities/interfaces/infrastructure/frameworks/ObjectLogGenerator.kt
+++ client/src/main/kotlin/com/lowes/auditor/client/entities/interfaces/infrastructure/frameworks/ObjectLogGenerator.kt
@@ -3,12 +3,12 @@ package com.lowes.auditor.client.entities.interfaces.infrastructure.frameworks
 import reactor.core.publisher.Mono
 
 /**
- * Object log generator interface containing function to generate log for any given object
+ * Object log generator interface containing function to generate audit log for any given object
  */
 interface ObjectLogGenerator {
 
     /**
-     * Generates a textual representation of an object (preferably json)
+     * Generates a textual representation of an object (mostly json except for when object type is string)
      * @param entity any object instance
      * @return mono of [String] containing textual representation
      */
