785957766ebb4877d5e512fd1abb9418a0d3a3aa
Deepankar Singh <deepankar.singh@lowes.com>
2022-01-31 15:39:39 +0530

    ktlint format

--- client/src/test/kotlin/com/lowes/auditor/client/infrastructure/frameworks/model/Item.kt
+++ client/src/test/kotlin/com/lowes/auditor/client/infrastructure/frameworks/model/Item.kt
@@ -3,26 +3,26 @@ package com.lowes.auditor.client.infrastructure.frameworks.model
 import java.math.BigDecimal
 
 data class Item(
-        val itemNumber: String? = null,
-        val model: Int? = null,
-        val description: String? = null,
-        val metadata: Map<String, String>? = null,
-        val rand: Rand? = null,
-        val rand2: Rand? = null,
-        val listItem: MutableList<Rand> ? = null,
-        val metadataRand: Map<String, Rand>? = null,
-        val stringList: List<String>? = null,
-        val price: BigDecimal? = null,
-        val subList: List<SubObject>? = null,
-        val subMap: Map<String, SubObject>? = null
+    val itemNumber: String? = null,
+    val model: Int? = null,
+    val description: String? = null,
+    val metadata: Map<String, String>? = null,
+    val rand: Rand? = null,
+    val rand2: Rand? = null,
+    val listItem: MutableList<Rand> ? = null,
+    val metadataRand: Map<String, Rand>? = null,
+    val stringList: List<String>? = null,
+    val price: BigDecimal? = null,
+    val subList: List<SubObject>? = null,
+    val subMap: Map<String, SubObject>? = null
 )
 
 data class Rand(
-        val id: String? = null,
-        val name: String? = null,
-        val doubleList: List<SubObject>? = null,
-        val listString: List<String>? = null,
-        val mapList: Map<String, List<SubObject>>? = null
+    val id: String? = null,
+    val name: String? = null,
+    val doubleList: List<SubObject>? = null,
+    val listString: List<String>? = null,
+    val mapList: Map<String, List<SubObject>>? = null
 )
 
 data class SubObject(
