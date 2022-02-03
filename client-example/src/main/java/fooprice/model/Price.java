5ad44dfa26628ad5b96cb57e23283ab162ce29e1
Deepankar Singh <deepankar.singh@lowes.com>
2022-02-03 12:24:14 +0530

    adding all scenarios to java standalone client example

--- client-example/src/main/java/fooprice/model/Price.java
+++ client-example/src/main/java/fooprice/model/Price.java
@@ -1,5 +1,6 @@
 package fooprice.model;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -8,12 +9,42 @@ public class Price {
     private Integer price;
     private String description;
     private Map<String, String> metadata;
+    private List<String> offers;
+    private PriceData data;
+    private String updatedBy;
 
-    public Price(UUID itemNumber, Integer price, String description, Map<String, String> metadata) {
+    public Price(UUID itemNumber, Integer price, String description, Map<String, String> metadata, List<String> offers, PriceData data, String updatedBy) {
         this.itemNumber = itemNumber;
         this.price = price;
         this.description = description;
         this.metadata = metadata;
+        this.offers = offers;
+        this.data = data;
+        this.updatedBy = updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public List<String> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(List<String> offers) {
+        this.offers = offers;
+    }
+
+    public PriceData getData() {
+        return data;
+    }
+
+    public void setData(PriceData data) {
+        this.data = data;
     }
 
     public void setItemNumber(UUID itemNumber) {
@@ -46,5 +77,21 @@ public class Price {
 
     public UUID getItemNumber() {
         return itemNumber;
+    }
+
+    public static class PriceData {
+        private String value;
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public PriceData(String value) {
+            this.value = value;
+        }
     }
 }
