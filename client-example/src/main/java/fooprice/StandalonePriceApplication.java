abebcb96c19cfa09aa53d978b8116e9857919327
Singh, Deepankar <deepankar.singh@lowes.com>
2022-02-03 02:58:45 -0500

    Pull request #13: Feature auditor demo
    Merge in E-MDM/auditor-v1 from feature-auditor-demo to master
    
    Squashed commit of the following:
    
    commit a1f094e6a8d1bd111ed0e2211b1dd93988cf66aa
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 13:11:03 2022 +0530
    
        commenting out integrationTest/functionalTest in CI until jenkins fixes test container docker issues
    
    commit 89423ac2840b01ca28ef6dab0411df288efcfdbe
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:37:30 2022 +0530
    
        adding named parameters to constructor
    
    commit aa7964e41b0d052c5e2fab8d89b6f4b307009689
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:28:06 2022 +0530
    
        spotless apply
    
    commit 7134c9043bd6afdc4d7e3da4d43cff997d6f9e42
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:27:35 2022 +0530
    
        ktlint formatter
    
    commit f8b96222a59b98ecdd9eef8587efbeb2c15f5a8c
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:24:51 2022 +0530
    
        changing to var for data classes to better java interoperability
    
    commit 07eb8f090eebcb0779838eb2e7d0371e7620e0c6
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:24:25 2022 +0530
    
        adding all scenarios to kotlin standalone client example
    
    commit 77b9876fb7d21ee07f0008cc004aff415ae1838c
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:24:14 2022 +0530
    
        adding all scenarios to java standalone client example
    
    commit 388b1a2285415e52d8054dbae90a59b5a8ff78bd
    Author: Deepankar Singh <deepankar.singh@lowes.com>
    Date:   Thu Feb 3 12:23:41 2022 +0530
    
        adding log4j2 for logging examples

--- client-example/src/main/java/fooprice/StandalonePriceApplication.java
+++ client-example/src/main/java/fooprice/StandalonePriceApplication.java
@@ -1,9 +1,13 @@
 package fooprice;
 
 import com.lowes.auditor.client.api.Auditor;
-import com.lowes.auditor.client.entities.domain.AuditorEventConfig;
+import com.lowes.auditor.client.entities.domain.*;
 import com.lowes.auditor.client.infrastructure.event.config.AuditEventProducerConfig;
+import com.lowes.auditor.core.entities.domain.EventSourceType;
+import com.lowes.auditor.core.entities.domain.EventType;
 import fooprice.model.Price;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -14,14 +18,152 @@ public class StandalonePriceApplication implements Runnable {
         this.auditor = auditor;
     }
 
+    private void testCreateOldObjectNullAndNewObject(UUID itemNumber) {
+        Price oldPrice = null;
+        Price newPrice = getPrice(itemNumber);
+        auditor.audit(oldPrice, newPrice);
+    }
+
+    private void testDeleteOldObjectNotNullAndNewObjectAsNull(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = null;
+        auditor.audit(oldPrice, newPrice);
+    }
+
+    private void testUpdateBothObject(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        newPrice.setPrice(50);
+        newPrice.setMetadata(Map.of("MetaKey", "metaValue", "NewMetaKey", "NewMetaValue"));
+        newPrice.setOffers(Collections.emptyList());
+        newPrice.setData(new Price.PriceData(""));
+        auditor.audit(oldPrice, newPrice);
+    }
+
+    private void testStaticDataSubstitution(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        AuditorEventConfig config = new AuditorEventConfig();
+        config.setApplicationName("client-example-java");
+        config.setEventSource(
+                new EventSourceConfig(
+                        EventSourceType.USER,
+                        new EventSourceMetadataConfig("static-user-id", null, null)));
+        config.setMetadata(Map.of("iteNumber", "Sadly i am only static-value"));
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testDynamicDataSubstitution(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        Price.PriceData priceData = new Price.PriceData("NewPriceDataValue");
+        newPrice.setData(priceData);
+        AuditorEventConfig config = new AuditorEventConfig();
+        config.setApplicationName("client-example-java");
+        config.setEventSource(
+                new EventSourceConfig(
+                        EventSourceType.USER,
+                        new EventSourceMetadataConfig("${updatedBy}", null, null)));
+        config.setMetadata(
+                Map.of(
+                        "iteNumber",
+                        "${itemNumber}",
+                        "price.data",
+                        "${data.value}",
+                        "static-key",
+                        "static-value"));
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testEventFilters(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        oldPrice.setOffers(null);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        newPrice.setPrice(50);
+        AuditorEventConfig config = new AuditorEventConfig();
+        EventFilter eventFilter =
+                new EventFilter(true, List.of(EventType.CREATED, EventType.UPDATED));
+        Filters filters = new Filters();
+        filters.setEvent(eventFilter);
+        config.setFilters(filters);
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testElementIncludesFilters(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        newPrice.setPrice(50);
+        AuditorEventConfig config = new AuditorEventConfig();
+        ElementFilterOptions options = new ElementFilterOptions();
+        options.setIncludes(List.of("description"));
+        ElementFilter elementFilter = new ElementFilter(true, List.of("InclusionFilter"), options);
+        Filters filters = new Filters();
+        filters.setElement(elementFilter);
+        config.setFilters(filters);
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testElementExcludesFilters(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        newPrice.setPrice(50);
+        AuditorEventConfig config = new AuditorEventConfig();
+        ElementFilterOptions options = new ElementFilterOptions();
+        options.setExcludes(List.of("description"));
+        ElementFilter elementFilter = new ElementFilter(true, List.of("ExclusionFilter"), options);
+        Filters filters = new Filters();
+        filters.setElement(elementFilter);
+        config.setFilters(filters);
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testLoggingFilters(UUID itemNumber) {
+        Price oldPrice = getPrice(itemNumber);
+        Price newPrice = getPrice(itemNumber);
+        newPrice.setDescription("new_price_description");
+        AuditorEventConfig config = new AuditorEventConfig();
+        Filters filters = new Filters();
+        filters.setLogging(new LoggingFilter(true));
+        config.setFilters(filters);
+        auditor.audit(oldPrice, newPrice, config);
+    }
+
+    private void testLog(UUID itemNumber) {
+        auditor.log(getPrice(itemNumber));
+    }
+
+    private Price getPrice(UUID itemNumber) {
+        Price.PriceData data = new Price.PriceData("PriceDataValue");
+        return new Price(
+                itemNumber,
+                1234,
+                "old_price_description",
+                Map.of("MetaKey", "metaValue"),
+                List.of("offers"),
+                data,
+                "DoctorStrange!");
+    }
+
     @Override
     public void run() {
         UUID itemNumber = UUID.randomUUID();
-        Price oldPrice = new Price(itemNumber, 1234, "old_price", null);
-        Price newPrice = new Price(itemNumber, 21212, "new_price", Map.of("new_price_id", "98767"));
-        System.out.println("Running auditor! for newItemNumber $newItemNumber");
-        auditor.audit(oldPrice, newPrice);
-        auditor.log("bigObject");
+        System.out.println("Running auditor! for" + itemNumber);
+        testCreateOldObjectNullAndNewObject(itemNumber);
+        testDeleteOldObjectNotNullAndNewObjectAsNull(itemNumber);
+        testUpdateBothObject(itemNumber);
+        testStaticDataSubstitution(itemNumber);
+        testDynamicDataSubstitution(itemNumber);
+        testEventFilters(itemNumber);
+        testElementIncludesFilters(itemNumber);
+        testElementExcludesFilters(itemNumber);
+        testLoggingFilters(itemNumber);
+        testLog(itemNumber);
         System.out.println("Done");
     }
 
